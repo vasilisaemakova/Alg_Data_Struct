5d5dbba2723ed9ba8a8c0149f07c8462b509f2e2
Ermakova Vasilisa <90772865+vasilisaemakova@users.noreply.github.com>
2023-11-09 04:42:29 +0300

    Delete lb_2/src directory

--- lb_2/src/Collections/MyQueue.java
+++
@@ -1,7 +0,0 @@
-package Collections;
-
-public interface MyQueue<T>{
-    boolean add(T data);
-    T poll();
-    boolean isEmpty();
-}
--- lb_2/src/Lab_2_1_2/Sorts/Sort.java
+++
@@ -1,5 +0,0 @@
-package Lab_2_1_2.Sorts;
-
-public interface Sort<T extends Comparable<T>> {
-    void sort();
-}
